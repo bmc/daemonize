a15cc8f3670143c9148aba92fa747ae127c2f51a
Brian Clapper <bmc@clapper.org>
2006-08-30 15:10:52 +0000

    Added basename emulator.

---
+++ basename.c
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------*\
+  $Id$
+
+  NAME
+	basename.c - replacement basename(3) function
+
+  DESCRIPTION
+
+        This source file contains a version of the basename() function.
+
+  LICENSE
+
+	This source code is released under the GNU Public License. See the
+        COPYING file for details.
+
+  Copyright (c) 2006 Brian M. Clapper, bmc <at> clapper <dot> org
+\*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*\
+                                 Includes
+\*---------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <string.h>
+
+/*---------------------------------------------------------------------------*\
+                              Public Routines
+\*---------------------------------------------------------------------------*/
+
+char *basename (char *path)
+{
+    char *s = strrchr (path, '/');
+
+    return (s == NULL) ? path : ++s;
+}
