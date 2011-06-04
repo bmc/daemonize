4f0905d8a8d8ff47d6b9f16dd76cf144030290ca
Brian Clapper <bmc@ardentex.com>
2011-06-03 23:23:55 -0400

    Add emulator for flock, for Solaris

---
+++ flock.c
@@ -0,0 +1,47 @@
+/*---------------------------------------------------------------------------*\
+  NAME
+	flock.c - flock(2), emulated (more or less) in terms of
+                  lockf
+
+  LICENSE
+
+	This source code is released under a BSD-style. See the LICENSE
+        file for details.
+
+  Copyright (c) 2011 Brian M. Clapper, bmc <at> clapper <dot> org
+\*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*\
+                                 Includes
+\*---------------------------------------------------------------------------*/
+
+#include <unistd.h>
+#include <errno.h>
+
+#ifdef HAVE_SYS_FILE_H
+#include <sys/file.h>
+#endif /* HAVE_SYS_FILE_H */
+
+#include "config.h"
+#include "flock.h"
+
+/*---------------------------------------------------------------------------*\
+                              Public Routines
+\*---------------------------------------------------------------------------*/
+
+int flock(int fd, int op)
+{
+    int cmd = 0;
+    errno = 0;
+
+    if (op & LOCK_UN)
+        cmd = F_ULOCK;
+    else if (op & LOCK_SH)  /* can't emulate shared lock with lockf() */
+        errno = EINVAL;
+    else if (op & LOCK_NB)
+        cmd = F_TLOCK;
+    else
+        cmd = F_LOCK;
+
+    return lockf(fd, cmd, 0);
+}
